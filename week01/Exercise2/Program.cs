5f05c2a214cef3b115e2083c56607758a1668d52
mattszymanski <tismusiccreations@gmail.com>
2025-05-09 00:38:49 -0600

    exercise 2 week 1 grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage= Console.ReadLine();
+        int number= int.Parse(gradePercentage);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (number >=60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+
+
 }
